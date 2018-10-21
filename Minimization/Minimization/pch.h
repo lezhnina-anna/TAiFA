eacebc76fafceb7478f64d385528ba31729fbaef
lezhnina-anna <Ankemova@gmail.com>
2018-10-21 14:15:27 +0300

    minimization

---
+++ Minimization/Minimization/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef PCH_H
+#define PCH_H
+
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/iteration_macros.hpp>
+#define _SCL_SECURE_NO_WARNINGS
+
+#endif //PCH_H
